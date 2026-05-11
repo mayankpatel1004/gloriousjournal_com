509837ef5a505377fe5ff7bd4b1c4e4a47d0a476
mayankpatel1004 <mayank.patel104@gmail.com>
2026-05-11 19:32:39 +0530

    Updates

--- online-submission.php
+++ online-submission.php
@@ -4,175 +4,174 @@ require_once 'phpmailer/class.phpmailer.php';
 require_once 'phpmailer/class.smtp.php';
 
     $upload_dir = $directory_path."uploads/";
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
-    // Form Data
-    $author_name      = $_POST['author_name'];
-    $email            = $_POST['email'];
-    $contact_no       = $_POST['contact_no'];
-    $institute        = $_POST['institute'];
-    $country          = $_POST['country'];
-    $co_authors       = $_POST['co_authors'];
-    $manuscript_title = $_POST['manuscript_title'];
+        // Form Data
+        $author_name      = $_POST['author_name'];
+        $email            = $_POST['email'];
+        $contact_no       = $_POST['contact_no'];
+        $institute        = $_POST['institute'];
+        $country          = $_POST['country'];
+        $co_authors       = $_POST['co_authors'];
+        $manuscript_title = $_POST['manuscript_title'];
 
-    // File Upload
-    $new_file_name = "";
-    $attachment = "";
-    if(isset($_FILES['manuscript_file']['name']) && $_FILES['manuscript_file']['name'] != ""){
-        $new_file_name = time() . "_" . basename($_FILES['manuscript_file']['name']);
-        $tmp_name      = $_FILES['manuscript_file']['tmp_name'];
+        // File Upload
+        $new_file_name = "";
+        $attachment = "";
+        if(isset($_FILES['manuscript_file']['name']) && $_FILES['manuscript_file']['name'] != ""){
+            $new_file_name = time() . "_" . basename($_FILES['manuscript_file']['name']);
+            $tmp_name      = $_FILES['manuscript_file']['tmp_name'];
 
-        // Upload Path
-        $upload_path = $upload_dir . $new_file_name;
+            // Upload Path
+            $upload_path = $upload_dir . $new_file_name;
 
-        // Move Uploaded File
-        move_uploaded_file($tmp_name, $upload_path);
-    }
-    
-
-    /*
-    |--------------------------------------------------------------------------
-    | INSERT INTO DATABASE
-    |--------------------------------------------------------------------------
-    */
-
-    $sql = "INSERT INTO manuscript_submissions
-    (
-        author_name,
-        email,
-        contact_no,
-        institute,
-        country,
-        co_authors,
-        manuscript_title,
-        manuscript_file
-    )
-    VALUES
-    (
-        :author_name,
-        :email,
-        :contact_no,
-        :institute,
-        :country,
-        :co_authors,
-        :manuscript_title,
-        :manuscript_file
-    )";
-
-    $stmt = $conn->prepare($sql);
-
-    $stmt->execute([
-        ':author_name'      => $author_name,
-        ':email'            => $email,
-        ':contact_no'       => $contact_no,
-        ':institute'        => $institute,
-        ':country'          => $country,
-        ':co_authors'       => $co_authors,
-        ':manuscript_title' => $manuscript_title,
-        ':manuscript_file'  => $new_file_name
-    ]);
-
-
-    /*
-    |--------------------------------------------------------------------------
-    | EMAIL VARIABLES
-    |--------------------------------------------------------------------------
-    */
-    if(isset($_FILES['manuscript_file']['name']) && $_FILES['manuscript_file']['name'] != ""){
-        
-        $attachment = $upload_path;
-    }
-
-    $admin_email   = "mayank.patel104@gmail.com";
-        $website_name  = "Your Website Name";
-
-        $subject = "New Manuscript Submission";
-
-        $message = '
-        <h2>New Manuscript Submission</h2>
-
-        <table border="1" cellpadding="10" cellspacing="0" width="100%">
-            <tr>
-                <td><strong>Author Name</strong></td>
-                <td>'.$author_name.'</td>
-            </tr>
-
-            <tr>
-                <td><strong>Email</strong></td>
-                <td>'.$email.'</td>
-            </tr>
-
-            <tr>
-                <td><strong>Contact No.</strong></td>
-                <td>'.$contact_no.'</td>
-            </tr>
-
-            <tr>
-                <td><strong>Institute/College</strong></td>
-                <td>'.$institute.'</td>
-            </tr>
-
-            <tr>
-                <td><strong>Country</strong></td>
-                <td>'.$country.'</td>
-            </tr>
-
-            <tr>
-                <td><strong>Co-authors</strong></td>
-                <td>'.$co_authors.'</td>
-            </tr>
-
-            <tr>
-                <td><strong>Manuscript Title</strong></td>
-                <td>'.$manuscript_title.'</td>
-            </tr>
-        </table>
-        ';
-
-    /*
-    |--------------------------------------------------------------------------
-    | SEND EMAIL
-    |--------------------------------------------------------------------------
-    */
-
-    try {
-
-        $mail = new PHPMailer(true);
-
-        $mail->isSMTP();
-        $mail->Host       = $smtp_host;
-        $mail->Port       = $stmp_port;
-        $mail->SMTPDebug  = 0;
-        $mail->SMTPAuth   = false;
-        $mail->Username   = $smtp_user;
-        $mail->Password   = $smtp_password;
-
-        $mail->SMTPSecure = false;
-
-        // Sender & Receiver
-        $mail->setFrom($smtp_user, $website_name);
-        $mail->addAddress($admin_email);
-
-        // Reply To
-        $mail->AddReplyTo($email, $author_name);
-
-        // Email Content
-        $mail->isHTML(true);
-        $mail->Subject = $subject;
-        $mail->MsgHTML($message);
-        if (isset($attachment) && $attachment != "") {
-            $mail->AddAttachment($attachment);
+            // Move Uploaded File
+            move_uploaded_file($tmp_name, $upload_path);
         }
-        $mail->Send();
-        header('Location:' . $url . 'online-submission.php?success=1');
-        exit;
+        
 
-    } catch (phpmailerException $e) {
-        echo $e->errorMessage();
-    } catch (Exception $e) {
-        echo $e->getMessage();
+        /*
+        |--------------------------------------------------------------------------
+        | INSERT INTO DATABASE
+        |--------------------------------------------------------------------------
+        */
+
+        $sql = "INSERT INTO manuscript_submissions
+        (
+            author_name,
+            email,
+            contact_no,
+            institute,
+            country,
+            co_authors,
+            manuscript_title,
+            manuscript_file
+        )
+        VALUES
+        (
+            :author_name,
+            :email,
+            :contact_no,
+            :institute,
+            :country,
+            :co_authors,
+            :manuscript_title,
+            :manuscript_file
+        )";
+
+        $stmt = $conn->prepare($sql);
+
+        $stmt->execute([
+            ':author_name'      => $author_name,
+            ':email'            => $email,
+            ':contact_no'       => $contact_no,
+            ':institute'        => $institute,
+            ':country'          => $country,
+            ':co_authors'       => $co_authors,
+            ':manuscript_title' => $manuscript_title,
+            ':manuscript_file'  => $new_file_name
+        ]);
+
+
+        /*
+        |--------------------------------------------------------------------------
+        | EMAIL VARIABLES
+        |--------------------------------------------------------------------------
+        */
+        if(isset($_FILES['manuscript_file']['name']) && $_FILES['manuscript_file']['name'] != ""){
+            
+            $attachment = $upload_path;
+        }
+
+            $admin_email   = "mayank.patel104@gmail.com";
+            $website_name  = "Glorious";
+
+            $subject = "New Manuscript Submission";
+
+            $message = '
+            <h2>New Manuscript Submission</h2>
+
+            <table border="1" cellpadding="10" cellspacing="0" width="100%">
+                <tr>
+                    <td><strong>Author Name</strong></td>
+                    <td>'.$author_name.'</td>
+                </tr>
+
+                <tr>
+                    <td><strong>Email</strong></td>
+                    <td>'.$email.'</td>
+                </tr>
+
+                <tr>
+                    <td><strong>Contact No.</strong></td>
+                    <td>'.$contact_no.'</td>
+                </tr>
+
+                <tr>
+                    <td><strong>Institute/College</strong></td>
+                    <td>'.$institute.'</td>
+                </tr>
+
+                <tr>
+                    <td><strong>Country</strong></td>
+                    <td>'.$country.'</td>
+                </tr>
+
+                <tr>
+                    <td><strong>Co-authors</strong></td>
+                    <td>'.$co_authors.'</td>
+                </tr>
+
+                <tr>
+                    <td><strong>Manuscript Title</strong></td>
+                    <td>'.$manuscript_title.'</td>
+                </tr>
+            </table>
+            ';
+
+        /*
+        |--------------------------------------------------------------------------
+        | SEND EMAIL
+        |--------------------------------------------------------------------------
+        */
+
+        try {
+
+            $mail = new PHPMailer(true);
+
+            $mail->isSMTP();
+            $mail->Host       = $smtp_host;
+            $mail->Port       = $stmp_port;
+            $mail->SMTPDebug  = 0;
+            $mail->SMTPAuth   = true;
+            $mail->Username   = $smtp_user;
+            $mail->Password   = $smtp_password;
+            $mail->SMTPSecure = 'tls';
+
+            // Sender & Receiver
+            $mail->setFrom($smtp_user, $website_name);
+            $mail->addAddress($admin_email);
+
+            // Reply To
+            $mail->AddReplyTo($email, $author_name);
+
+            // Email Content
+            $mail->isHTML(true);
+            $mail->Subject = $subject;
+            $mail->MsgHTML($message);
+            if (isset($attachment) && $attachment != "") {
+                $mail->AddAttachment($attachment);
+            }
+            $mail->Send();
+            header('Location:' . $url . 'online-submission.php?success=1');
+            exit;
+
+        } catch (phpmailerException $e) {
+            echo $e->errorMessage();
+        } catch (Exception $e) {
+            echo $e->getMessage();
+        }
     }
-}
 
 ?>
 <!DOCTYPE html>
--- temp.php
+++ temp.php
@@ -1,191 +1,103 @@
 <?php
 
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
+ini_set('display_errors', 1);
 
-require 'PHPMailer/src/Exception.php';
-require 'PHPMailer/src/PHPMailer.php';
-require 'PHPMailer/src/SMTP.php';
+require_once 'phpmailer/class.phpmailer.php';
+require_once 'phpmailer/class.smtp.php';
 
+// SMTP CONFIG
+$admin_email = "mayank.patel104@gmail.com";
+$smtp_host = "smtp.hostinger.com";
+$stmp_port = 587;
+$smtp_user = "notifications@cloudswiftsolutions.com";
+$smtp_password = "Cloud@112018";
+$website_name  = "Cloudons";
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+// FORM DATA
+$name         = "Mayank";
+$email        = "mayankp@yopmail.com";
+$mobile       = "09090909";
+$req_message  = "Msg";
 
-    // Form Data
-    $author_name      = $_POST['author_name'];
-    $email            = $_POST['email'];
-    $contact_no       = $_POST['contact_no'];
-    $institute        = $_POST['institute'];
-    $country          = $_POST['country'];
-    $co_authors       = $_POST['co_authors'];
-    $manuscript_title = $_POST['manuscript_title'];
+$subject = "Contact Inquiry";
 
-    // File Upload
-    $new_file_name = time() . "_" . basename($_FILES['manuscript_file']['name']);
-    $tmp_name      = $_FILES['manuscript_file']['tmp_name'];
+// MESSAGE BODY
+$message  = "";
+$message .= "Name : " . $name . "<br />";
+$message .= "Email : " . $email . "<br />";
+$message .= "Contact : " . $mobile . "<br />";
+$message .= "Message : " . $req_message . "<br />";
 
-    // Upload Path
-    $upload_path = $upload_dir . $new_file_name;
+try {
 
-    // Move Uploaded File
-    move_uploaded_file($tmp_name, $upload_path);
+    $mail = new PHPMailer(true);
 
+    // SMTP SETTINGS
+    $mail->IsSMTP();
+
+    $mail->Host       = $smtp_host;
+    $mail->Port       = $stmp_port;
+
+    $mail->SMTPAuth   = true;
+
+    $mail->Username   = $smtp_user;
+    $mail->Password   = $smtp_password;
+
+    // TLS
+    $mail->SMTPSecure = 'tls';
+
+    // OPTIONAL SSL FIX
+    $mail->SMTPOptions = array(
+        'ssl' => array(
+            'verify_peer'       => false,
+            'verify_peer_name'  => false,
+            'allow_self_signed' => true
+        )
+    );
+
+    // FROM
+    $mail->From     = $smtp_user;
+    $mail->FromName = $website_name;
+
+    // TO
+    $mail->AddAddress($admin_email);
+
+    // REPLY TO
+    $mail->AddReplyTo($email, $name);
+
+    // EMAIL FORMAT
+    $mail->IsHTML(true);
+
+    // SUBJECT
+    $mail->Subject = $subject;
+
+    // BODY
+    $mail->Body = $message;
+
+    // ATTACHMENT
     /*
-    |--------------------------------------------------------------------------
-    | INSERT INTO DATABASE
-    |--------------------------------------------------------------------------
-    */
-
-    $sql = "INSERT INTO manuscript_submissions
-    (
-        author_name,
-        email,
-        contact_no,
-        institute,
-        country,
-        co_authors,
-        manuscript_title,
-        manuscript_file
-    )
-    VALUES
-    (
-        :author_name,
-        :email,
-        :contact_no,
-        :institute,
-        :country,
-        :co_authors,
-        :manuscript_title,
-        :manuscript_file
-    )";
-
-    $stmt = $conn->prepare($sql);
-
-    $stmt->execute([
-        ':author_name'      => $author_name,
-        ':email'            => $email,
-        ':contact_no'       => $contact_no,
-        ':institute'        => $institute,
-        ':country'          => $country,
-        ':co_authors'       => $co_authors,
-        ':manuscript_title' => $manuscript_title,
-        ':manuscript_file'  => $new_file_name
-    ]);
-
-
-    /*
-    |--------------------------------------------------------------------------
-    | EMAIL VARIABLES
-    |--------------------------------------------------------------------------
-    */
-
-    $admin_email   = "admindfdsfdsfsdaf@gmail.com";
-    $website_name  = "Your Website Name";
-
-    $smtp_host     = "smtp.gmail.com";
-    $stmp_port     = 587;
-    $smtp_user     = "yourgmail@gmail.com";
-    $smtp_password = "your_app_password";
-
-    $subject = "New Manuscript Submission";
-
-    $message = '
-    <h2>New Manuscript Submission</h2>
-
-    <table border="1" cellpadding="10" cellspacing="0" width="100%">
-        <tr>
-            <td><strong>Author Name</strong></td>
-            <td>'.$author_name.'</td>
-        </tr>
-
-        <tr>
-            <td><strong>Email</strong></td>
-            <td>'.$email.'</td>
-        </tr>
-
-        <tr>
-            <td><strong>Contact No.</strong></td>
-            <td>'.$contact_no.'</td>
-        </tr>
-
-        <tr>
-            <td><strong>Institute/College</strong></td>
-            <td>'.$institute.'</td>
-        </tr>
-
-        <tr>
-            <td><strong>Country</strong></td>
-            <td>'.$country.'</td>
-        </tr>
-
-        <tr>
-            <td><strong>Co-authors</strong></td>
-            <td>'.$co_authors.'</td>
-        </tr>
-
-        <tr>
-            <td><strong>Manuscript Title</strong></td>
-            <td>'.$manuscript_title.'</td>
-        </tr>
-    </table>
-    ';
-
-    // Attachment
-    $attachment = $upload_path;
-
-
-    /*
-    |--------------------------------------------------------------------------
-    | SEND EMAIL
-    |--------------------------------------------------------------------------
-    */
-
-    try {
-
-        $mail = new PHPMailer(true);
-
-        $mail->isSMTP();
-        $mail->Host       = $smtp_host;
-        $mail->Port       = $stmp_port;
-        $mail->SMTPDebug  = 0;
-        $mail->SMTPAuth   = true;
-        $mail->Username   = $smtp_user;
-        $mail->Password   = $smtp_password;
-
-        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-
-        // Sender & Receiver
-        $mail->setFrom($smtp_user, $website_name);
-        $mail->addAddress($admin_email);
-
-        // Reply To
-        $mail->AddReplyTo($email, $author_name);
-
-        // Email Content
-        $mail->isHTML(true);
-        $mail->Subject = $subject;
-        $mail->MsgHTML($message);
-
-        // Attachment
-        if (isset($attachment) && $attachment != "") {
-            $mail->AddAttachment($attachment);
-        }
-
-        // Send Email
-        $mail->Send();
-
-        // Redirect
-        header('Location:' . $url . 'online-submission.php?success=1');
-        exit;
-
-    } catch (phpmailerException $e) {
-
-        echo $e->errorMessage();
-        exit;
-
-    } catch (Exception $e) {
-
-        echo $e->getMessage();
-        exit;
+    if(isset($attachment) && $attachment != ""){
+        $mail->AddAttachment($attachment);
     }
+    */
+
+    // SEND EMAIL
+    if ($mail->Send()) {
+
+        echo "Email Sent Successfully";
+
+    } else {
+
+        echo $mail->ErrorInfo;
+    }
+
+} catch (phpmailerException $e) {
+
+    echo $e->errorMessage();
+
+} catch (Exception $e) {
+
+    echo $e->getMessage();
 }
+
 ?>
