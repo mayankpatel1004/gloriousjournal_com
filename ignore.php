4239accbcf8bfc3487171ff74f58b9763c928852
mayankpatel1004 <mayank.patel104@gmail.com>
2026-05-11 19:07:06 +0530

    Updates

---
+++ ignore.php
@@ -0,0 +1,5 @@
+$admin_email = "notifications@cloudswiftsolutions.com";
+$smtp_host = "smtp.hostinger.com";
+$stmp_port = 587;
+$smtp_user = "notifications@cloudswiftsolutions.com";
+$smtp_password = "Cloud@112018";
